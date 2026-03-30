03683dccf51875b736efe4a2b01c8bc5f1ae454a
Phanvangiu <tvtphanvangiu@gmail.com>
2026-03-30 15:35:57 +0700

    chore(FobiddenException): Create file ForbiddenException

---
+++ backend/src/VocabToeic.Application/Common/Exceptions/ForbiddenException.cs
@@ -0,0 +1,10 @@
+namespace VocabToeic.Application.Common.Exceptions;
+
+/// <summary>
+/// Exception thrown when a user is authenticated but does not have permission to access a resource.
+/// Usage: throw new ForbiddenException("You do not have permission to perform this action.")
+/// </summary>
+public class ForbiddenException : Exception
+{
+  public ForbiddenException(string message) : base(message) { }
+}
